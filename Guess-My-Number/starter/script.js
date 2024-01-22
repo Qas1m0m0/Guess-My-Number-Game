a2d92dce979e25b8418731dde2a4080d319381e5
Qas1m0m0 <Qasim.17@hotmail.com>
2024-01-22 14:21:37 +0000

    Refactored code- final

--- Guess-My-Number/starter/script.js
+++ Guess-My-Number/starter/script.js
@@ -46,26 +46,16 @@ function resetGame() {
             document.querySelector('.highscore').textContent = highscore;
         }
     } 
-    // When guess is too high
-    else if(guess > secretNumber) {
-        if(score > 1){
-        document.querySelector('.message').textContent = 'Too High!'
-        score--;
-        document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = 'You Lost The Game!';
-            document.querySelector('.score').textContent = 0
-    }
-    } 
-       // When gues is too low
-        else if(guess < secretNumber) {
-        if(score > 1){
-            document.querySelector('.message').textContent = 'Too Low!'
+
+    //  // When gues is wrong:
+     else if (guess !== secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = guess > secretNumber ? 'Too High!' : 'Too Low!';
             score--;
             document.querySelector('.score').textContent = score;
-            } else {
-                document.querySelector('.message').textContent = 'You Lost The Game!';
-                document.querySelector('.score').textContent = 0;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
+     }
     }
-}
-}
